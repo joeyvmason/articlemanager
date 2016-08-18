3dedae5769859b76f1037f662d1978d7e26bf7de
joeyvmason <joeyvmason@gmail.com>
2016-08-18 15:25:10 -0700

    Use ISO8061 for dates

--- core/src/main/java/com/joeyvmason/articlemanager/core/application/CoreConfig.java
+++ core/src/main/java/com/joeyvmason/articlemanager/core/application/CoreConfig.java
@@ -2,6 +2,7 @@ package com.joeyvmason.articlemanager.core.application;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -15,6 +16,7 @@ public class CoreConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().build();
+        objectMapper.setDateFormat(new ISO8601DateFormat());
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         return objectMapper;
     }
